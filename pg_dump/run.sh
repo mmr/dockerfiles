8d11f5becc087a992be13ce7194f7784a4b83e04
Marcio Ribeiro <marcio@loggi.com>
2015-10-30 17:28:39 -0200

    [update] 1) letting user pass params in a more organized way 2) making it possible to pass out file 3) gziping outfile

--- pg_dump/run.sh
+++ pg_dump/run.sh
@@ -1,13 +1,41 @@
-#!/bin/sh
-db_user="$1"
-db_host="$2"
-db_name="$3"
-out_file="/data/dump-$(date +%Y%m%d).sql"
+#!/bin/bash -e
+usage() {
+    name=${0##*/}
+    echo "Usage: $name"
+    echo "-u|--user: db user, required"
+    echo "-h|--host: db host, required"
+    echo "-d|--db  : db name, required"
+    echo "-f|--file: name of the dump file to be created, optional"
+    echo "           a file with the current date will be created if none is given"
+    echo "-z|--gzip: to compress the output file with gzip, optional"
+    exit
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -u|--user) db_user="$2"; shift;;
+        -h|--host) db_host="$2"; shift;;
+        -d|--db)   db_name="$2"; shift;;
+        -f|--file) out_file="$2"; shift;;
+        -z|--gzip) gzip=yes;;
+        *) usage;;
+    esac
+    shift
+done
 
 if [ -z $db_user ] || [ -z $db_host ] || [ -z $db_name ]; then
-  echo "Usage: $0 db_user db_host db_name"
-  exit
+    usage
 fi
 
-echo "Dumping '$db_name' as '$db_user@$db_host'..."
+if [ -z $out_file ]; then
+    out_file="dump-$(date +%Y%m%d).sql"
+fi
+out_file="/data/$out_file"
+
+echo "Dumping '$db_name' as '$db_user@$db_host' to '$out_file'..."
 pg_dump -U "$db_user" -h "$db_host" "$db_name" > "$out_file"
+
+if ! [ -z "$gzip" ]; then
+    echo "Compressing $out_file..."
+    gzip -9 "$out_file"
+fi
